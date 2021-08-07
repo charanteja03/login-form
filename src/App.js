1eef5a78d9aa6875c03042a13bc56a93dbf9ce1f
Charan Teja Koneru <charanteja03@gmail.com>
2021-08-07 11:18:17 +0530

    Basic login form

--- src/App.js
+++ src/App.js
@@ -1,22 +1,78 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import axios from 'axios';
 
 function App() {
+  const [username, setUsername] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
+  const handleLoginSubmit = () => {
+    alert('login detected ' + username + ' ' + password);
+    axios.get('login').then(response => {
+      console.log('logged in')
+    }).catch(e => {
+      console.log(e);
+    })
+    console.log('clicked login');
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <section className="login-section" style={{
+          backgroundColor: 'white',
+          color: 'black',
+          width: '300px',
+          height: '300px',
+          borderRadius: '10px',
+          padding: '15px'
+        }}>
+          <div>Login Form</div>
+          <form onSubmit={handleLoginSubmit}>
+            <div className="form-group" style={{
+              display: 'flex',
+              alignItems: 'flex-start',
+              justifyContent: 'flex-start',
+              flexDirection: 'column',
+              margin: '15px'
+            }}>
+              <label className="control-label" htmlFor="username-input" style={{
+                fontSize: '0.85rem'
+              }}>Username</label>
+              <input type="username" className="form-control" value={username} onChange={e => setUsername(e.target.value)} id="username-input" 
+              placeholder="Username"
+              style={{
+                height: '30px',
+                width: '10vw',
+                minWidth: '200px'
+              }}/>
+            </div>
+            <div className="form-group" style={{
+              display: 'flex',
+              alignItems: 'flex-start',
+              justifyContent: 'flex-start',
+              flexDirection: 'column',
+              margin: '15px'
+            }}>
+              <label className="control-label" htmlFor="username-input" style={{
+                fontSize: '0.85rem'
+              }}>Password</label>
+              <input type="password" className="form-control" value={password} onChange={e => setPassword(e.target.value)} id="password-input" 
+              placeholder="Password"
+              style={{
+                height: '30px',
+                width: '10vw',
+                minWidth: '200px'
+              }}/>
+            </div>
+            <div>
+              <button type="submit" onClick={handleLoginSubmit} style={{
+                width: '80%',
+                height: '30px'
+              }}>Login</button>
+            </div>
+          </form>
+        </section>
       </header>
     </div>
   );
